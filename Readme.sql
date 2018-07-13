71317b7b4139090d6a59b0ee8c02832f39d553ff
Roberto Rocuant <r.rocuant4@hotmail.com>
2018-07-12 20:29:00 -0400

    README
    Información del curso

---
+++ Readme.sql
@@ -0,0 +1,13 @@
+/*
+Este curso es sobre MySQL
+
+-Conceptos básicos.
+-Entidad relación.
+-Modelado en Workbench.
+-Instrucciones básicas.
+-Distintos tipos de joins.
+-Count.
+-Sum, Max, Min, Avg.
+-Subconsultas.
+-Transacciones.
+*/
